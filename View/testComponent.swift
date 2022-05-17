80aa49a55af1633601d5babc6cbeedb7361baaab
redspy <minsu.medison@gmail.com>
2022-05-17 23:04:46 +0900

    Image Resizable and VStack align center

--- View/testComponent.swift
+++ View/testComponent.swift
@@ -13,16 +13,18 @@ struct testComponent: View {
         nameText = "HanSoul"
     }
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
+        VStack(alignment: .center, spacing: 10) {
             
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             Spacer()
             Text(nameText)
-//            Image("pad")
-//                .clipShape(Circle())
-//                .frame(width: 100, height: 100)
-//                .shadow(radius: 10)
-//                .padding(.bottom)
+
+            Image("pad")
+                .resizable()
+                .clipShape(Circle())
+                .frame(width: 100, height: 100)
+                .shadow(radius: 10)
+                .padding(.bottom)
             Button("Press", action: ButtonAction)
                 
             Spacer()
@@ -45,6 +47,9 @@ struct testComponent: View {
 
 struct testComponent_Previews: PreviewProvider {
     static var previews: some View {
-        testComponent()
+        Group {
+            testComponent()
+            testComponent()
+        }
     }
 }
